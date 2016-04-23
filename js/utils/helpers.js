28457af0f71994a6b641a432b70f9ad48edbef88
marpi <marpi@roygbi.ws>
2016-04-23 14:41:21 -0700

    class

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -127,10 +127,10 @@ function getCubeMap(i) {
 /*
  * 
  
-    hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+    var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
     hemiLight.color.setHSL( 0.6, 1, 0.6 );
     hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-    hemiLight.position.set( 0, 500, 0 );
+    hemiLight.position.set( 0, 0, 0 );
     scene.add( hemiLight );
  
  * 
