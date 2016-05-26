eb48cff32057af3d7d70611be9fe7d5776b9dd91
TortillaGuy <agcacho22@gmail.com>
2016-05-25 18:02:10 -0700

    fix path to envMaps assets for users'

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -60,7 +60,7 @@ function getCubeMap(i) {
     var loader = new THREE.ImageLoader();
     var pre = "assets/textures/";
     if (user)
-        pre = "../assets/textures/";
+        pre = "../../assets/textures/";
     var file = pre + envMaps[i].file;
     var size = envMaps[i].size;
     loader.load(file, function (image) {
